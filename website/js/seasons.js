499b16b66ea880556860e071045aacd68e9fa444
Gustav <gubo0001@stud.kea.dk>
2023-09-08 11:38:46 +0200

    js start

---
+++ website/js/seasons.js
@@ -0,0 +1,5 @@
+// https://dsokyrsukcwokvaqfpmr.supabase.co
+
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzb2t5cnN1a2N3b2t2YXFmcG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MTg1NTksImV4cCI6MjAwOTM5NDU1OX0.jRejMqwnT2Rn9EPXZU2A5fGtxabNC6MeJOi2Zm97xCk
+
+
