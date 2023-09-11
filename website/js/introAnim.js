ef4cab0135c555d3030dbf0fa2a8e43c829827d0
Ossie <osvald.rye@gmail.com>
2023-09-11 15:03:01 +0200

    introAnimV1

---
+++ website/js/introAnim.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", introAnim);
+
+function introAnim(){
+    document.querySelector("#middleground").classList.add("midGrndAnim")
+    document.querySelector("#foreground").classList.add("forGrndAnim")
+    document.querySelector("#headerNav").classList.add("navAnim")
+}
+
+
+
+// <div id="background">
+// </div>
+// <div id="middleground">
+// </div>
+// <div id="foreground">
